15be0f12445d5fef0f07c07bff840d44e56728fe
unknown <khushishu3031@gmail.com>
2023-01-01 19:29:05 +0530

    problem2

---
+++ problem2.js
@@ -0,0 +1,13 @@
+var reg_email="abc@gmail.com";
+var reg_pass="123456";
+
+let ent_email="abc@gmail.com";
+let ent_pass="123456";
+
+if(ent_email==reg_email);
+if(ent_pass==reg_pass);{
+  console.log("log in successful");
+}'else'
+{
+  console.log("incorrect password")
+}
